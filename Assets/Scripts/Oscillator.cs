eb28845547e3fb38749fe85f4f5ed25a2dbc04a1
Arktikus <ArktikusArkan@gmail.com>
2022-10-26 05:19:15 +0200

    Added the Oscillator
    (Framework for Moving Blocks).

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 8f;
+
     void Start()
     {
-
+        startingPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period; // continually growing over time
 
+        const float tau = Mathf.PI * 2; // constant value of 6.283..
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go form 0 to 1 so its clear
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
     }
 }
